dbdad78126933902cd77cb54ac8f36b87a3d958c
Angel Silva <angel.arsd@gmail.com>
2018-11-07 16:21:01 -0300

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Roboto');
+    font-family: 'Roboto', sans-serif;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <GlobalStyles />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
